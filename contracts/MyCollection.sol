8a2b972bd7f3e52d05487b4baac1d5f1c104cbeb
nazaawa <johnnyetsha@gmail.com>
2023-02-21 16:57:21 +0100

    frist init

--- contracts/MyCollection.sol
+++ contracts/MyCollection.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+import "@thirdweb-dev/contracts/extension/Permissions.sol";
+
 
 contract Contract is ERC721Base {
-
+    // Any `bytes32` value is a valid role. You can create roles by defining them like this.
+    bytes32 public constant NICKNAME_ROLE = keccak256("NICKNAME_ROLE");
       constructor(
         string memory _name,
         string memory _symbol,
@@ -17,6 +20,19 @@ contract Contract is ERC721Base {
             _royaltyRecipient,
             _royaltyBps
         )
-    {}
+    {
+            
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(NICKNAME_ROLE, msg.sender);
+    }
+
+
+    // mapping of token Id to nickname
+    mapping (uint256 => string) tokenNickname;
+
+    //upload the value of the mapping for a token Id
+    function updateTokenNickname(uint256 , string calldata _nickname) public onlyRole() {
+
+    }
 
 }
